52fbb5acd13d7c076073268b1beb406d7bb82098
Theodore Marshall <ted@tedm.net>
2026-01-31 12:43:13 -0800

    -Add excerpt to project cards, add comments to grid and modules css

--- page-portfolio.php
+++ page-portfolio.php
@@ -50,7 +50,7 @@ get_header(); ?>
 							<span class="h3-project-bg-top"></span>
 							<span class="h3-project-bg-bottom"></span>
 						</div>
-						<p>Amazing new content that will fill this space for future viewing.</p>
+						<p><?php the_excerpt() ?></p>
 						<hr>
 					</a>
 					<?php the_tags( '<p class="tags"><span class="tags-title screen-reader-text">' . __( 'Tags:', 'bonestheme' ) . '</span> ', '<span class="screen-reader-text">,</span> ', '</p>' ); ?>
